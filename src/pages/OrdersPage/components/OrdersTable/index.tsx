0363c223d8760cdc52fdd4d78ff60661b9824273
hoangbinhan <hoangbinhana3@gmail.com>
2020-11-25 00:56:56 +0700

    update pagination waiting for fixing

--- src/pages/OrdersPage/components/OrdersTable/index.tsx
+++ src/pages/OrdersPage/components/OrdersTable/index.tsx
@@ -1,36 +1,49 @@
 //libs
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Table } from 'antd'
 import { useDispatch } from 'react-redux'
+import queryString from "query-string";
 //components
 import OrdersFilter from '../OrdersFilter'
 //hooks
 import { useTypedSelector, useRouter } from '@/hooks'
 //action
-import {fetchDataOrders} from '@/actions/Order/FetchDataOrders'
+import { fetchDataOrders } from '@/actions/Order/FetchDataOrders'
 //others
 import { columns } from '../../DataSource/OrdersTable'
 
 const OrdersTable = () => {
-    const dispatch = useDispatch()
     const router = useRouter()
-    const {listOrders, isLoading} = useTypedSelector(
-        (state)=>state.Order.FetchDataOrders
+    const dispatch = useDispatch()
+    const { listOrders, isLoading } = useTypedSelector(
+        (state) => state.Order.FetchDataOrders
     )
-    const {updateSuccess} = useTypedSelector(
-        (state)=>state.Order.UpdateStatusOrder
-    )    
+    const { updateSuccess } = useTypedSelector(
+        (state) => state.Order.UpdateStatusOrder
+    )
     useEffect(() => {
-        dispatch(fetchDataOrders({params: router.query}))
+        dispatch(fetchDataOrders({ params: router.query }))
     }, [dispatch, updateSuccess, router.query])
 
-    const data = listOrders?.map((item:any)=>{
-        return {...item, key: item._id}
+    const data = listOrders?.map((item: any) => {
+        return { ...item, key: item._id }
     })
     return (
         <>
-            <OrdersFilter/>
-            <Table columns={columns} dataSource={data} loading={isLoading}/>
+            <OrdersFilter />
+            <Table columns={columns} dataSource={data} loading={isLoading} pagination={{
+                // total: listProducts?.total,
+                // pageSize: listProducts?.size,
+                // current: listProducts?.page + 1,
+                showSizeChanger: true,
+                pageSizeOptions: ["10", "20", "30", "50", "100"],
+                onChange: (page, pageSize) => {
+                    const currentParam = { ...router.query, page, size: pageSize };
+                    router.push(
+                        `${router.pathname}?${queryString.stringify(currentParam)}`
+                    );
+                },
+            }} />
         </>
     )
 }
